eccc0ff7ba0d10a06a08e195afeb6ef1b49b1fae
WondrousSquirrel <wondroussquirrel@gmail.com>
2019-03-30 18:29:29 +0300

    заменены санки на саги

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
 
 import rootReducer from "./reducers/index";
 
-const middleware = [thunk];
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./saga/saga";
 
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [sagaMiddleware];
 const store = createStore(
   rootReducer,
   compose(
@@ -12,5 +14,7 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
